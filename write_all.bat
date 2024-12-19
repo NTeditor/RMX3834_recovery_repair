227ca71cf069db94da51dcafe80f1a10b2a55549
NTeditor <nteditor4@gmail.com>
2024-12-19 17:08:24 +0300

    v0.1

---
+++ write_all.bat
@@ -0,0 +1,2 @@
+spd_dump exec_addr 0x65015f08 fdl fdl1-dl.bin 0x65000800 fdl fdl2-dl.bin 0x9efffe00 exec write_part boot_a boot.img write_part boot_b boot.img write_part vendor_boot_a vendor_boot.img write_part vendor_boot_b vendor_boot.img reset
+pause
